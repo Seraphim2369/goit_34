36dcf673e5c42c3bd5165be3b0b7047c8eb80d49
Oksana20020605 <chybchik@ukr.net>
2026-04-09 21:21:57 +0300

    зменшила n_permutations до 100

--- python/ab_tests.py
+++ python/ab_tests.py
@@ -45,7 +45,7 @@ def permutation_test(df1, df2, n_permutations):
 
     return p_value
 
-n_permutations = 2000
+n_permutations = 100
 
 df1 = treatment['converted_int']
 df2 = control['converted_int']
