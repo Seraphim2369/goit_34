65b84137666b70de0e3123c77b45e9469013ade1
ADMIN <vabaysara@gmail.com>
2026-04-09 20:54:47 +0300

    else:     print("We accept null hypothesis. There is no significant difference between the two groups.") delete

--- python/ab_tests.py
+++ python/ab_tests.py
@@ -56,8 +56,6 @@ print(f'P-Value: {p_value}')
 
 if p_value < 0.05:
     print("We reject null hypothesis. There is a significant difference between the two groups.")
-else:
-    print("We accept null hypothesis. There is no significant difference between the two groups.")
 
 
 #%%
